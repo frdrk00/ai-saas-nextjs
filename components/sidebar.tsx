b8e5b53514c403e0f29d708fa288a1e285899c5e
frdrk00 <masterkooo@gmail.com>
2023-08-01 23:18:54 +0200

    API Limit UI Counter

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,6 +15,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import FreeCounter from '@/components/free-counter'
+
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] })
 
 const routes = [
@@ -61,8 +63,12 @@ const routes = [
   },
 ]
 
-const Sidebar = () => {
-const pathname = usePathname()
+interface SidebarProps {
+  apiLimitCount: number
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
+  const pathname = usePathname()
 
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -80,8 +86,11 @@ const pathname = usePathname()
             <Link
               href={route.href}
               key={route.href}
-              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", 
-              pathname === route.href ? "bg-white/10 text-white" : "text-zinc-400"
+              className={cn(
+                'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                pathname === route.href
+                  ? 'bg-white/10 text-white'
+                  : 'text-zinc-400'
               )}
             >
               <div className="flex items-center flex-1 ">
@@ -92,6 +101,9 @@ const pathname = usePathname()
           ))}
         </div>
       </div>
+      <FreeCounter 
+      apiLimitCount={apiLimitCount}
+      />
     </div>
   )
 }
